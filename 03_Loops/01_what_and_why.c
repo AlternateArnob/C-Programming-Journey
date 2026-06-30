1ea693d3a7286d40ed41784707371e6f6c92d583
AlternateArnob <alternatearnob@gmail.com>
2026-06-30 19:16:36 +0600

    feat: start lecture 03 with loop introduction and repetition concepts

---
+++ 03_Loops/01_what_and_why.c
@@ -0,0 +1,25 @@
+// What and Why?
+
+// Loop is used for repetition.
+
+// Lets assume we have to print Hello World 100 times:
+// printf("Hello World");
+// printf("Hello World");
+// printf("Hello World");
+// ...
+// ...
+// ...
+
+// Will we do this 100 times?
+
+// Nope. We follow the DRY principle.
+// DRY -> Don't Repeat Yourself.
+
+// This is where loops become useful.
+
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
