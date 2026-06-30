636768a84e30083b2e63e62a0db8816c017e44d1
AlternateArnob <alternatearnob@gmail.com>
2026-06-30 19:22:23 +0600

    feat: add star diamond pattern

---
+++ 04_Pattern_Printing/29_star_diamond.c
@@ -0,0 +1,51 @@
+// Ques: Print the given pattern
+
+/*
+          *
+        * * *
+      * * * * *
+    * * * * * * *
+      * * * * *
+        * * *
+          *
+*/
+
+// Star Diamond Pattern
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number of rows (in odd): ");
+    scanf("%d", &n);
+
+    int nsp = n / 2;
+    int nst = 1;
+    int mid = n / 2 + 1;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= nsp; j++) // spaces
+        {
+            printf("  ");
+        }
+        for(int k = 1; k <= nst; k++) // stars
+        {
+            printf("* ");
+        }
+
+        if(i < mid)
+        {
+            nsp--;
+            nst += 2; // nst = nst + 2
+        }
+        else
+        {
+            nsp++;
+            nst -= 2; // nst = nst - 2
+        }
+        printf("\n");
+    }
+    return 0;
+}
