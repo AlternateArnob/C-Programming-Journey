4d9b590d9b062ab7ff5853352476c560cc502ab3
AlternateArnob <alternatearnob@gmail.com>
2026-06-30 19:20:38 +0600

    feat: start lecture 04 with pattern printing introduction

---
+++ 04_Pattern_Printing/01_pattern_printing_intro.c
@@ -0,0 +1,40 @@
+// Pattern Printing - Introduction
+
+// Ques: Print the given pattern
+
+/*
+    * * * * *
+    * * * * *
+    * * * * *
+*/
+
+// Solid Rectangle Pattern
+
+#include <stdio.h>
+
+int main()
+{
+    // Hardcoded approach:
+
+    printf("* * * * *\n");
+    printf("* * * * *\n");
+    printf("* * * * *\n");
+
+    // But this is not the real idea behind pattern printing.
+
+    // What if:
+    // Number of rows = n
+    // Number of columns = m
+    // Both values are taken from the user.
+
+    // Example:
+    // Create a solid rectangle of stars using n rows and m columns.
+
+    // Writing printf statements manually will not work.
+
+    // This is where loops become useful.
+    // Pattern printing usually requires nested loops.
+
+    // Nested loops -> a loop inside another loop
+    return 0;
+}
