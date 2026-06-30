5ba2576bac6ae49cf32cc1e800a2ea19c3f09b20
AlternateArnob <alternatearnob@gmail.com>
2026-06-30 19:07:01 +0600

    feat: start lecture 01 with Hello World program

---
+++ 01_Basics/01_hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World"); // printf function
+    return 0;
+}
+
+// Output:
+// Hello World
+
+// Any text written inside the double quotation marks ("") of the printf() function
+// will be displayed on the screen.
