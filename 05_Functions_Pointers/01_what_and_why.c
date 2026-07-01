9b6adb7051b522712b776efe0bbee94ebf9a85f9
AlternateArnob <alternatearnob@gmail.com>
2026-07-01 15:19:55 +0600

    feat: start lecture 05 with functions introduction

---
+++ 05_Functions_Pointers/01_what_and_why.c
@@ -0,0 +1,27 @@
+// In mathematics:
+// y = f(x) = x^2 + 5
+// Here, y is a function of x.
+
+// In computer science:
+// The main purpose of functions is to avoid code repetition.
+
+// What is a function?
+// A reusable block of code that performs a specific task
+// and can be used repeatedly throughout a program.
+
+// Why do we use functions?
+// 1. To avoid code repetition.
+
+    // We already use loops to repeat statements.
+    // However, loops repeat statements at the same place in the program.
+    // Functions help us reuse the same block of code wherever it is needed.
+    // This is a different kind of repetition than what loops solve.
+
+// 2. To make the code more readable and organized.
+
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
